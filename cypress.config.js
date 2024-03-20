1cb6c9f5e155753954ea83666b76c9ef8daa8ede
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-03-20 11:01:02 -0300

    add lighthouse plugin and test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("cypress");
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
+//const { pa11y } = require("@cypress-audit/pa11y");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
@@ -6,6 +8,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on)
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+
+      on("task", {
+        lighthouse: lighthouse(),
+        //pa11y: pa11y(console.log.bind(console)),
+      });
     },
   },
 });
