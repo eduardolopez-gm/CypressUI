414ff2e266c18eba79eb989ea3d9e9f07321e8c8
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-03-21 16:27:38 -0300

    add parallel testing and scripts to generate report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
-
+const { beforeRunHook } = require('cypress-mochawesome-reporter/lib')
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on)
+      on('before:run', async (details) => {
+        await beforeRunHook(details);
+      })
     },
   },
 });
