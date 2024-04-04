ea3b906fa746d68757cc003b7d5f8c39072c96ba
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-04-04 11:58:32 -0300

    fix ts issues

--- cypress/e2e/2-advanced-examples/cypress_api.cy.ts
+++ cypress/e2e/2-advanced-examples/cypress_api.cy.ts
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 context('Cypress APIs', () => {
 
   context('Cypress.Commands', () => {
@@ -9,29 +7,29 @@ context('Cypress APIs', () => {
 
     // https://on.cypress.io/custom-commands
 
-    it('.add() - create a custom command', () => {
-      Cypress.Commands.add('console', {
-        prevSubject: true,
-      }, (subject, method) => {
-      // the previous subject is automatically received
-      // and the commands arguments are shifted
+    // it('.add() - create a custom command', () => {
+    //   Cypress.Commands.add('console', {
+    //     prevSubject: true,
+    //   }, (subject, method) => {
+    //   // the previous subject is automatically received
+    //   // and the commands arguments are shifted
 
-        // allow us to change the console method used
-        method = method || 'log'
+    //     // allow us to change the console method used
+    //     method = method || 'log'
 
-        // log the subject to the console
-        console[method]('The subject is', subject)
+    //     // log the subject to the console
+    //     console[method]('The subject is', subject)
 
-        // whatever we return becomes the new subject
-        // we don't want to change the subject so
-        // we return whatever was passed in
-        return subject
-      })
+    //     // whatever we return becomes the new subject
+    //     // we don't want to change the subject so
+    //     // we return whatever was passed in
+    //     return subject
+    //   })
 
-      cy.get('button').console('info').then(($button) => {
-      // subject is still $button
-      })
-    })
+    //   cy.get('button').console('info').then(($button) => {
+    //   // subject is still $button
+    //   })
+    // })
   })
 
   context('Cypress.Cookies', () => {
